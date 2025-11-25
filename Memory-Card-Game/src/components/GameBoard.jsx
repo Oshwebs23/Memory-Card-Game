ffb2edf7bf84296f5928c36e4f47cef15e57b882
Oshwebs23 <osian1996@live.co.uk>
2025-11-25 16:03:46 +0000

    add comment for GameBoard.jsx purpose

---
+++ Memory-Card-Game/src/components/GameBoard.jsx
@@ -0,0 +1,10 @@
+// GameBoard.jsx
+// Purpose: Acts as the referee of the Memory Card Game.
+// - Renders the grid of Card components.
+// - Tracks two choosen cards (firstChoice, secondChoice).
+// - Compares them to check for matches.
+// - Calls back to App.jsx to update score and matched state
+// - Resets choices after each turn.
+// - In Short: GameBoard manages game flow, while Card.jsx handles individual card flip logic. 
+
+
